84e5687bdc422f93bd0e0492379bc90a4526c481
Flavio De Stefano <destefano.flavio@gmail.com>
2015-06-23 00:27:34 +0200

    Seq generic

--- seq-p0.js
+++ seq-p0.js
@@ -1,20 +1,22 @@
 var π = require('./index.js');
 var fs = require('fs');
 
+var N = 13;
+
 (function _next(number) {
 
-	var number_str = new Array(7 - number.toString().length).join('0') + number.toString();
+	var number_str = new Array((N+1) - number.toString().length).join('0') + number.toString();
 	process.stdout.write('Searching ' + number_str + ': ');
 
 	π.find(number_str, function(status) {
 		if (status.error) {
-			fs.appendFileSync('seq6-p0.txt', number + "\n");
+			fs.appendFileSync('seq' + N + '-p0.txt', '' + number + "\n");
 			process.stdout.write("NOT FOUND\n");
 		} else {
 			process.stdout.write(''+status.index+"\n");
 		}
 
-		if (++number === 1000000) {
+		if (++number === Math.pow(10, N)) {
 			process.exit();
 		} else {
 			_next(number);
