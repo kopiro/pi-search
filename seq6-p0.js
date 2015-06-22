a5726d034b98b43e4ae3aba70aace2ae13ebd90e
Flavio De Stefano <destefano.flavio@gmail.com>
2015-06-22 22:08:12 +0200

    file write

--- seq6-p0.js
+++ seq6-p0.js
@@ -1,16 +1,20 @@
 var π = require('./index.js');
+var fs = require('fs');
 
 (function _next(number) {
 
 	var number_str = new Array(7 - number.toString().length).join('0') + number.toString();
+	process.stdout.write('Searching ' + number_str + ': ');
 
 	π.find(number_str, function(status) {
 		if (status.error) {
-			process.stdout.write(number+"\n");
+			fs.appendFileSync('seq6-p0.txt', number + "\n");
+			process.stdout.write('NOT FOUND');
+		} else {
+			process.stdout.write(''+status.index+"\n");
 		}
 
 		if (++number === 1000000) {
-			process.stdout.write('We reached the end');
 			process.exit();
 		} else {
 			_next(number);
