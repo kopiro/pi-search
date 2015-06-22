f6fd15e5ab28d734f097246f14da2cc4f3bab67a
Flavio De Stefano <destefano.flavio@gmail.com>
2015-06-22 21:24:28 +0200

    Old buffer

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 exports.find = function(to_find, callback) {
-	var stream = require('fs').createReadStream('pi-billion.txt', { start: 2 });
-
+	// Check if is a number
 	if (!/^\d+$/.test(to_find)) {
 		return callback({
 			error: true,
@@ -8,17 +7,29 @@ exports.find = function(to_find, callback) {
 		});
 	}
 
-	stream.on('data', function(buffer) {
-		var index_of = buffer.toString().indexOf(process.argv[2]);
+	var stream = require('fs').createReadStream('pi-billion.txt', {
+		start: 2
+	});
+
+	var old_buffer_length = -1 * (to_find.toString().length - 1);
+	var old_ending_buffer_str = '';
+
+	stream
+	.on('data', function(buffer) {
+		var buffer_str = old_ending_buffer_str + buffer.toString();
+
+		var index_of = buffer_str.indexOf(process.argv[2]);
 		if (index_of >= 0) {
 			return callback({
 				success: true,
 				index: index_of
 			});
 		}
-	});
 
-	stream.on('end', function() {
+		old_ending_buffer_str = buffer_str.substr(old_buffer_length);
+	})
+
+	.on('end', function() {
 		return callback({
 			error: true,
 			message: 'Not found'
